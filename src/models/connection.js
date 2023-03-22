034820c60e768d5769af1f048dcf8e51eae1824c
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-03-22 10:29:11 -0300

    renomeia variaveis de ambiente, para deploy railway

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  port: process.env.MYSQL_PORT || 3306,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT || 3306,
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'password',
+  database: process.env.MYSQLDATABASE || 'StoreManager',
 });
 
 module.exports = connection;
